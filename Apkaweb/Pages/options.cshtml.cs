d6f8542475cd9648017630d9c894d5bdaf8d9f87
jwroblewska <105785046+jwroblewska@users.noreply.github.com>
2024-04-05 22:57:34 +0200

    Update options.cshtml.cs

--- Apkaweb/Pages/options.cshtml.cs
+++ Apkaweb/Pages/options.cshtml.cs
@@ -1,23 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
-using System.Threading.Tasks;
 
 namespace Apkaweb.Pages
 {
-    [Authorize] // Require authentication to access this page
+    [Authorize] // autoryzacja
     public class optionsModel : PageModel
     {
         public async Task<IActionResult> OnPostLogoutAsync()
         {
-            await HttpContext.SignOutAsync(); // Sign out the user
-            return RedirectToPage("/Index"); // Redirect to the home page after logout
+            await HttpContext.SignOutAsync();
+            return RedirectToPage("/Index");
         }
 
-        public IActionResult OnPostShowMessagesAsync() => RedirectToPage("/ShowMessages"); // Redirect to ShowMessages page
+        public IActionResult OnPostShowMessagesAsync() => RedirectToPage("/ShowMessages");
 
-        public IActionResult OnPostPermissionsAsync() => RedirectToPage("/Permissions"); // Redirect to Permissions page
+        public IActionResult OnPostPermissionsAsync() => RedirectToPage("/Permissions");
     }
 }
