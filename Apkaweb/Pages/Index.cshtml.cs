b25294f0dc2e83d0ee5d2d4436631b08eed9f3a2
jwroblewska <105785046+jwroblewska@users.noreply.github.com>
2024-04-06 00:53:26 +0200

    Update Index.cshtml.cs

--- Apkaweb/Pages/Index.cshtml.cs
+++ Apkaweb/Pages/Index.cshtml.cs
@@ -1,60 +1,152 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Security.Claims;
 
-    using Microsoft.AspNetCore.Authentication;
-    using Microsoft.AspNetCore.Authentication.Cookies;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Mvc.RazorPages;
-    using Microsoft.Extensions.Configuration;
-    using MySql.Data.MySqlClient;
-    using System;
-    using System.Security.Claims;
-    using System.Threading.Tasks;
-
-    namespace Apkaweb.Pages
+namespace Apkaweb.Pages
+{
+    public class IndexModel : PageModel
     {
-        public class IndexModel : PageModel
-        {
-            private readonly IConfiguration _configuration;
+        private readonly IConfiguration _configuration;
 
-            public IndexModel(IConfiguration configuration)
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (User.Identity.IsAuthenticated)
             {
-                _configuration = configuration;
+                return RedirectToPage("/Options");
             }
 
-            public async Task<IActionResult> OnPostAsync(string login, string password)
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string login, string password)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (var connection = new MySqlConnection(connectionString))
             {
-                string connectionString = _configuration.GetConnectionString("DefaultConnection");
+                await connection.OpenAsync();
 
-                using (var connection = new MySqlConnection(connectionString))
+                bool isBlocked = await IsAccountBlocked(connection, login);
+                bool isBlockEnabled = await IsBlockEnabled(connection, login);
+                if (isBlocked)
                 {
-                    await connection.OpenAsync();
+                    return RedirectToPage("/LockedAccount", new { username = login });
+                }
 
-                    string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
-                    using (var command = new MySqlCommand(query, connection))
+                string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", login);
+                    command.Parameters.AddWithValue("@Password", password);
+                    int count = Convert.ToInt32(await command.ExecuteScalarAsync());
+
+                    if (count > 0)
                     {
-                        command.Parameters.AddWithValue("@Username", login);
-                        command.Parameters.AddWithValue("@Password", password);
-                        int count = Convert.ToInt32(await command.ExecuteScalarAsync());
-
-                        if (count > 0)
+                        // resetowanie liczby nie udanych prób logowania przy poprawnym logowaniu
+                        if (isBlockEnabled)
                         {
-                           
-                            var claims = new[]
-                            {
-                                new Claim(ClaimTypes.Name, login),
-                                new Claim(ClaimTypes.Role, "User")
-                            };
+                            await ResetFailedLoginAttempts(connection, login);
+                        }
 
-                            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                        var claims = new[]
+                        {
+                            new Claim(ClaimTypes.Name, login),
+                            new Claim(ClaimTypes.Role, "User")
+                        };
 
-                            
-                            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-                            
-                            return RedirectToPage("/Options");
+                        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+                        return RedirectToPage("/Options");
+                    }
+                    else
+                    {
+                        if (isBlockEnabled)
+                        {
+                            await IncrementFailedLoginAttempts(connection, login);
                         }
                     }
                 }
-                return Page();
+            }
+            return Page();
+        }
+
+        private async Task ResetFailedLoginAttempts(MySqlConnection connection, string username)
+        {
+            string query = "UPDATE Users SET FailedLoginAttempts = 0 WHERE Username = @Username";
+            using (var command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Username", username);
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+
+        private async Task<bool> IsAccountBlocked(MySqlConnection connection, string username)
+        {
+            string query = "SELECT IsBlockEnabled, FailedLoginAttempts, NumberOfAttempts FROM Users WHERE Username = @Username";
+            using (var command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Username", username);
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (reader.Read())
+                    {
+                        int isBlockEnabled = reader.GetInt32("IsBlockEnabled");
+                        int failedLoginAttempts = reader.GetInt32("FailedLoginAttempts");
+                        int numberOfAttempts = reader.GetInt32("NumberOfAttempts");
+                        return isBlockEnabled == 1 && (numberOfAttempts > 0 && failedLoginAttempts >= numberOfAttempts);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        private async Task IncrementFailedLoginAttempts(MySqlConnection connection, string username)
+        {
+            string query = "UPDATE Users SET FailedLoginAttempts = FailedLoginAttempts + 1 WHERE Username = @Username";
+            using (var command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Username", username);
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+
+        private async Task<bool> IsBlockEnabled(MySqlConnection connection, string username)
+        {
+            string query = "SELECT IsBlockEnabled FROM Users";
+            using (var command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Username", username);
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (reader.Read())
+                    {
+                        int isBlockEnabled = reader.GetInt32("IsBlockEnabled");
+                        if (isBlockEnabled == 1)
+                        {
+                            return true;
+                        }
+                        else return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
             }
         }
     }
+}
