1a871f8d5bbe9d0d3d4f0f3eb903ab99f21863ab
jwroblewska <105785046+jwroblewska@users.noreply.github.com>
2024-04-06 12:37:28 +0200

    Update and rename showMessages.cshtml.cs to ShowMessages.cshtml.cs

--- Apkaweb/Pages/ShowMessages.cshtml.cs
+++ Apkaweb/Pages/ShowMessages.cshtml.cs
@@ -58,67 +58,6 @@ namespace Apkaweb.Pages
             }
         }
 
-        private async Task LoadMessagesAsync()
-        {
-            Messages.Clear();
-
-            string connectionString = _configuration.GetConnectionString("DefaultConnection");
-
-            using (var connection = new MySqlConnection(connectionString))
-            {
-                await connection.OpenAsync();
-
-                string query = "SELECT * FROM Messages";
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    using (var reader = await command.ExecuteReaderAsync())
-                    {
-                        while (await reader.ReadAsync())
-                        {
-                            var message = new Message
-                            {
-                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                                Content = reader.GetString(reader.GetOrdinal("Content")),
-                                UserId = reader.GetString(reader.GetOrdinal("UserId"))
-                            };
-                            Messages.Add(message);
-                        }
-                    }
-                }
-            }
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                await LoadMessagesAsync();
-                return Page();
-            }
-
-            string userId = User.Identity.Name;
-
-            string connectionString = _configuration.GetConnectionString("DefaultConnection");
-
-            using (var connection = new MySqlConnection(connectionString))
-            {
-                await connection.OpenAsync();
-
-                string insertQuery = "INSERT INTO Messages (Content, UserId) VALUES (@Content, @UserId)";
-                using (var command = new MySqlCommand(insertQuery, connection))
-                {
-                    command.Parameters.AddWithValue("@Content", NewMessage);
-                    command.Parameters.AddWithValue("@UserId", userId);
-
-                    await command.ExecuteNonQueryAsync();
-
-                    await LoadMessagesAsync();
-
-                    return RedirectToPage("/ShowMessages");
-                }
-            }
-        }
-
         public async Task<IActionResult> OnPostDeleteAsync(int messageId)
         {
             string loggedInUserId = User.Identity.Name; // pobranie nazwy zalogowanego użytkownika
@@ -195,6 +134,67 @@ namespace Apkaweb.Pages
                 }
             }
             return RedirectToPage("/ShowMessages");
+        }
+
+        private async Task LoadMessagesAsync()
+        {
+            Messages.Clear();
+
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                string query = "SELECT * FROM Messages";
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var message = new Message
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Content = reader.GetString(reader.GetOrdinal("Content")),
+                                UserId = reader.GetString(reader.GetOrdinal("UserId"))
+                            };
+                            Messages.Add(message);
+                        }
+                    }
+                }
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                await LoadMessagesAsync();
+                return Page();
+            }
+
+            string userId = User.Identity.Name;
+
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                string insertQuery = "INSERT INTO Messages (Content, UserId) VALUES (@Content, @UserId)";
+                using (var command = new MySqlCommand(insertQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Content", NewMessage);
+                    command.Parameters.AddWithValue("@UserId", userId);
+
+                    await command.ExecuteNonQueryAsync();
+
+                    await LoadMessagesAsync();
+
+                    return RedirectToPage("/ShowMessages");
+                }
+            }
         }
 
         private async Task<Message> GetMessageByIdAsync(int messageId)
